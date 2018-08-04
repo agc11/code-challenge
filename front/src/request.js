aab0b9e241fe2b21a2c5ce14b9cf543a773cb284
agc11 <adrian11gomez@gmail.com>
2018-08-04 13:27:54 +0200

    dockerizeee :rocket:

--- front/src/request.js
+++ front/src/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function(query) {
   return new Promise((resolve, reject) => {
-    axios.post('http://localhost:4000/graphql', { query })
+    axios.post(`${process.env.REACT_APP_API}/graphql`, { query })
       .then(response => resolve(response.data))
       .catch(error => reject(error));
   });
